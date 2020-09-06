d040e371132b7f5134c6e450739853a1a3159016
Dmitry Fedotov <dm.fe@yandex.ru>
2020-09-06 22:06:03 +0300

    Change dmenu style for shutdown script

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 font="Monospace-18"
+panel_background=$(xrdb -query | grep '*.panel_background' | awk '{print $NF}')
 
 pgrep -x dmenu && exit
 
-chosen=$(echo -e "Shutdown\nReboot" | dmenu -fn "${font}" -i -p "What would you like to do?")
+chosen=$(echo -e "Shutdown\nReboot" | dmenu -fn "${font}" -nb "${panel_background}" -i -p "What would you like to do?")
 if [[ "${chosen}" = Shutdown ]]; then
-    yesno=$(echo -e "Yes\nNo" | dmenu -fn "${font}" -i -p "Do you really want to shutdown?")
+    yesno=$(echo -e "Yes\nNo" | dmenu -fn "${font}" -nb "${panel_background}" -i -p "Do you really want to shutdown?")
     [[ "${yesno}" = Yes ]] && sudo shutdown -h now
 elif [[ "${chosen}" = Reboot ]]; then
-    yesno=$(echo -e "Yes\nNo" | dmenu -fn "${font}" -i -p "Do you really want to reboot?")
+    yesno=$(echo -e "Yes\nNo" | dmenu -fn "${font}" -nb "${panel_background}" -i -p "Do you really want to reboot?")
     [[ "${yesno}" = Yes ]] && sudo reboot
 fi
