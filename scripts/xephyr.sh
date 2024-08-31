ba433492f1c295e0ddd7464de723cdefe65af50a
Dmitry Fedotov <dm.fe@yandex.ru>
2024-08-31 17:39:41 +0300

    Modiry xephyr script.

--- scripts/xephyr.sh
+++ scripts/xephyr.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 Xephyr -br -ac -reset -screen 1920x1080 :5 &
-# Xephyr -br -ac -reset -screen 800x600 :1 &
 sleep 1s
-
 export DISPLAY=:5
-
-# wallpaper 20 &
-xrdb ~/.Xresources
 $1 &
 
